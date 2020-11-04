5523c118c05d2ccaf11d06419eec0289aac83540
unknown <ysulton@yahoo.com>
2020-11-03 18:12:54 -0600

    pushing current work

---
+++ README.md
@@ -0,0 +1,1 @@
+This is Yolanda, I am a software Enginerr for my own company. Please respect all the rules in these websites. Thank you
---
+++ quizapp/question.js
@@ -0,0 +1,57 @@
+ var questions = [{
+    "question": "When is the emergency exit used ?",
+    "option1": "when front door is blocked" ,
+    "option2": "when there is an extreme emergency",
+    "option3": "when there is a fire ",
+    "option4": "all of above",
+    "answer": "4"
+}, {
+    "question": "where is it illegal to pass ?",
+    "option1": "hills" ,
+    "option2": "intersectons",
+    "option3": "railroad crossings",
+    "option4": "all of the above",
+    "answer": "4"
+}, {
+    "question": "Who has the responsibility of setting up passenger stops?",
+    "option1": "the president of usa " ,
+    "option2": "the parent",
+    "option3": "the driver",
+    "option4": "transportation department",
+    "answer": "4"
+}, {
+    "question": "what emergency equipment must you carry on the bus ?",
+    "option1": "A bat" ,
+    "option2": "A fire extinguisher and first aid",
+    "option3": "accident report form",
+    "option4": "All of the above",
+    "answer": "2"
+}, {
+    "question": "A passenger who is misbehaving can be put off and made to walk ?",
+    "option1": "as determinded by bus driver" ,
+    "option2": "as determined by parent",
+    "option3": "never",
+    "option4": "as determinded by the police",
+    "answer": "3"
+}, {
+    "question": "Allowing students to ride a bus is  ?",
+    "option1": "a mandate" ,
+    "option2": "a privalege",
+    "option3": "a human right",
+    "option4": "a act of charity",
+    "answer": "2"
+}, {
+    "question": "Which is never prohibited on a school bus?",
+    "option1": "a gun" ,
+    "option2": "gasoline",
+    "option3": "babies",
+    "option4": "all the above",
+    "answer": "4"
+}, {
+    "question": "Whose responsibility is it to maintain good disipline at all times on the bus ?",
+    "option1": "the bus diver" ,
+    "option2": "parents",
+    "option3": "truant officer",
+    "option4": "teachers",
+    "answer": "1"
+}];
